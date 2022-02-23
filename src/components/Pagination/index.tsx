f7544251a134067502b0b614c01230130754255c
Lucas Sacoman <lucas.sousa@devio.com.br>
2022-02-22 23:16:34 -0300

    refactory: refactoring code

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
+import { PaginationItem } from "./PaginationItem";
 
 const Pagination = () => {
 	return (
@@ -8,52 +9,12 @@ const Pagination = () => {
 			</Box>
 
 			<HStack spacing="2">
-				<Button
-					size="sm"
-					fontSize="xs"
-					width="4"
-					colorScheme="pink"
-					disabled
-					_disabled={{
-						bg: "pink.500",
-						cursor: "default",
-					}}
-				>
-					1
-				</Button>
-				<Button
-					size="sm"
-					fontSize="xs"
-					width="4"
-					bg="gray.700"
-					_hover={{
-						bg: "gray.500",
-					}}
-				>
-					2
-				</Button>
-				<Button
-					size="sm"
-					fontSize="xs"
-					width="4"
-					bg="gray.700"
-					_hover={{
-						bg: "gray.500",
-					}}
-				>
-					3
-				</Button>
-				<Button
-					size="sm"
-					fontSize="xs"
-					width="4"
-					bg="gray.700"
-					_hover={{
-						bg: "gray.500",
-					}}
-				>
-					4
-				</Button>
+				<PaginationItem number={1} isCurrent />
+				<PaginationItem number={2} />
+				<PaginationItem number={3} />
+				<PaginationItem number={4} />
+				<PaginationItem number={5} />
+				<PaginationItem number={6} />
 			</HStack>
 		</HStack>
 	);
---
+++ src/components/Sidebar.tsx/NavLink.tsx
@@ -0,0 +1,20 @@
+import { Icon, Link, LinkProps, Text } from "@chakra-ui/react";
+import { ElementType } from "react";
+
+interface NavLinkProps extends LinkProps {
+	icon: ElementType;
+	children: string;
+}
+
+const NavLink = ({ icon, children, ...rest }: NavLinkProps) => {
+	return (
+		<Link display="flex" alignItems="center" {...rest}>
+			<Icon as={icon} fontSize="20" />
+			<Text ml="4" fontWeight="medium">
+				{children}
+			</Text>
+		</Link>
+	);
+};
+
+export { NavLink };
