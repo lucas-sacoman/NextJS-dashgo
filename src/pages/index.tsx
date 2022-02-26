a6b4da88d7c2c2ba7060ef578153790f2d72d25b
Lucas Sacoman <lucas.sousa@devio.com.br>
2022-02-26 13:43:08 -0300

    feat: added react hook form on singIn page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
 import { Flex, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Button } from "../components/Form/Button";
 import { Input } from "../components/Form/Input";
+import { SignInData } from "../types";
 
 const Home: NextPage = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { isSubmitting },
+	} = useForm<SignInData>();
+
+	const handleSignIn: SubmitHandler<SignInData> = async (values) => {
+		await new Promise((resolve) => setTimeout(resolve, 1000 * 2));
+
+		console.log(values);
+	};
+
 	return (
 		<Flex w="100vw" h="100vh" align="center" justify="center">
 			<Flex
@@ -14,13 +28,19 @@ const Home: NextPage = () => {
 				bg="gray.800"
 				p="8"
 				flexDir="column"
+				onSubmit={handleSubmit(handleSignIn)}
 			>
 				<Stack spacing="4">
-					<Input type="email" name="email" label="E-mail" />
-					<Input type="password" name="password" label="Senha" />
+					<Input type="email" label="E-mail" {...register("email")} />
+					<Input type="password" label="Senha" {...register("password")} />
 				</Stack>
 
-				<Button innerText="Entrar" color="pink" />
+				<Button
+					type="submit"
+					innerText="Entrar"
+					color="pink"
+					isLoading={isSubmitting}
+				/>
 			</Flex>
 		</Flex>
 	);
