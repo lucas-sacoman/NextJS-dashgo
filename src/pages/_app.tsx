678e8c072128b13bb505cfed5252b8b1d5679f76
Lucas Sacoman <lucas.sousa@devio.com.br>
2022-03-05 17:09:58 -0300

    feat: added miragejs api, added react query and get users of api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,28 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
-import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
-import { theme } from "../styles/theme";
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
+import { theme } from '../styles/theme';
+
+if (process.env.NODE_ENV === 'development') {
+	makeServer();
+}
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider theme={theme}>
-			<SidebarDrawerProvider>
-				<Component {...pageProps} />
-			</SidebarDrawerProvider>
-		</ChakraProvider>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider theme={theme}>
+				<SidebarDrawerProvider>
+					<Component {...pageProps} />
+				</SidebarDrawerProvider>
+			</ChakraProvider>
+
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,8 @@ import {
 	Flex,
 	Heading,
 	Icon,
+	Spinner,
+	Stack,
 	Table,
 	Tbody,
 	Td,
@@ -13,14 +15,46 @@ import {
 	Thead,
 	Tr,
 	useBreakpointValue,
-} from "@chakra-ui/react";
-import Link from "next/link";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
-import { Sidebar } from "../../components/Sidebar.tsx";
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { RiAddLine, RiErrorWarningLine, RiPencilLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
+import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
+import { Sidebar } from '../../components/Sidebar.tsx';
+
+interface User {
+	id: number;
+	name: string;
+	email: string;
+	createdAt: string;
+}
 
 const UserList = () => {
+	const fetchUsers = async () => {
+		const response = await fetch('http://localhost:3000/api/users');
+		const data = await response.json();
+
+		const users: User[] = data.users.map((user: User) => {
+			return {
+				id: user.id,
+				name: user.name,
+				email: user.email,
+				createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+					day: '2-digit',
+					month: 'long',
+					year: 'numeric',
+				}),
+			};
+		});
+
+		return users;
+	};
+
+	const { isLoading, error, data } = useQuery('users', fetchUsers, {
+		staleTime: 1000 * 5, // 5 seconds
+	});
+
 	const isWideVersion = useBreakpointValue({ base: false, lg: true });
 
 	return (
@@ -49,47 +83,69 @@ const UserList = () => {
 						</Link>
 					</Flex>
 
-					<Table colorScheme="whiteAlpha">
-						<Thead>
-							<Tr>
-								<Th px={["0", "0", "8"]} color="gray.300" width="8">
-									<Checkbox colorScheme="pink" />
-								</Th>
-								<Th>Usuário</Th>
-								{isWideVersion && <Th>Data de cadastro</Th>}
-								{isWideVersion && <Th w="8"></Th>}
-							</Tr>
-						</Thead>
-						<Tbody>
-							<Tr>
-								<Td px={["0", "0", "8"]}>
-									<Checkbox colorScheme="pink" />
-								</Td>
-								<Td>
-									<Box>
-										<Text fontWeight="bold">Lucas Sacoman</Text>
-										<Text fontSize="sm">lucas.sacoman1999@gmail.com</Text>
-									</Box>
-								</Td>
-								{isWideVersion && <Td>21 de feveiro, 2022</Td>}
-								{isWideVersion && (
-									<Td>
-										<Button
-											as="a"
-											size="sm"
-											fontSize="sm"
-											colorScheme="purple"
-											leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
-										>
-											Editar
-										</Button>
-									</Td>
-								)}
-							</Tr>
-						</Tbody>
-					</Table>
+					{isLoading ? (
+						<Flex justify="center" align="center">
+							<Spinner />
+						</Flex>
+					) : error ? (
+						<Stack
+							justify="center"
+							align="center"
+							direction="column"
+							color="red.400"
+						>
+							<Icon as={RiErrorWarningLine} w="10" h="10" />
+							<Text>Erro ao listar usuários</Text>
+						</Stack>
+					) : (
+						<>
+							<Table colorScheme="whiteAlpha">
+								<Thead>
+									<Tr>
+										<Th px={['0', '0', '8']} color="gray.300" width="8">
+											<Checkbox colorScheme="pink" />
+										</Th>
+										<Th>Usuário</Th>
+										{isWideVersion && <Th>Data de cadastro</Th>}
+										{isWideVersion && <Th w="8"></Th>}
+									</Tr>
+								</Thead>
+								<Tbody>
+									{data!.map((user) => (
+										<Tr key={user.id}>
+											<Td px={['0', '0', '8']}>
+												<Checkbox colorScheme="pink" />
+											</Td>
+											<Td>
+												<Box>
+													<Text fontWeight="bold">{user.name}</Text>
+													<Text fontSize="sm" color="gray.400">
+														{user.email}
+													</Text>
+												</Box>
+											</Td>
+											{isWideVersion && <Td>{user.createdAt}</Td>}
+											{isWideVersion && (
+												<Td>
+													<Button
+														as="a"
+														size="sm"
+														fontSize="sm"
+														colorScheme="purple"
+														leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
+													>
+														Editar
+													</Button>
+												</Td>
+											)}
+										</Tr>
+									))}
+								</Tbody>
+							</Table>
 
-					<Pagination />
+							<Pagination />
+						</>
+					)}
 				</Box>
 			</Flex>
 		</Box>
